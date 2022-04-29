d9a3297ed221a2528b4a3c23e11df76f8729de13
wondsn <kimgs0725@gmail.com>
2022-04-29 16:43:07 +0900

    security-tutorial ph2
      - reference: https://gaemi606.tistory.com/entry/Spring-Boot-Spring-Security-OAuth2-2?category=745027
      - authenticationManager를 빈으로 만들때 함수명을 `authenticationManager`로 하면 Stackoverflow 에러 발생
        - https://stackoverflow.com/questions/58598448/why-authenticationmanager-is-throwing-stackoverflowerror 참조

--- src/main/java/hello/wondsn/securityturotial/config/OAuth2AuthorizationConfig.java
+++ src/main/java/hello/wondsn/securityturotial/config/OAuth2AuthorizationConfig.java
@@ -1,13 +1,24 @@
 package hello.wondsn.securityturotial.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
+@RequiredArgsConstructor
 public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -15,9 +26,10 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
                 .withClient("clientId")
                 .secret("{noop}clientSecret")
                 .redirectUris("http://localhost:8081/oauth2/callback")
-                .authorizedGrantTypes("authorization_code")
+                .authorizedGrantTypes("authorization_code", "password")
                 .scopes("read", "write")
                 .accessTokenValiditySeconds(60 * 60 * 4)
-                .refreshTokenValiditySeconds(60 * 60 * 24 * 120);
+                .refreshTokenValiditySeconds(60 * 60 * 24 * 120)
+                .autoApprove(true);
     }
 }
